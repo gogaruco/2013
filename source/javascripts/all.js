2f43cb25cc11b516438f8ad14eb3851f10815d40
James Hart <hjhart@gmail.com>
2013-05-16 23:22:01 -0700

    Counter now displays accurate days until  - (who cares about timezones when there are 120+ days?)

--- source/javascripts/all.js
+++ source/javascripts/all.js
@@ -1,3 +1,11 @@
 //= require jquery
 //= require foundation
 //= require_tree .
+
+(function() {
+  var conference_date = new Date(2013, 8, 20);
+  var today = new Date();
+  var one_day_in_milliseconds = (1000 * 60 * 24 * 60);
+  var days_until_conference = Math.ceil((conference_date - today) / one_day_in_milliseconds);
+  $('#days_until_conference #number').html(days_until_conference);
+})()
