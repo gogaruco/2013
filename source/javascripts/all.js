e090b0fec0efccb35b8a8d6729e5194e67f170ad
Abhi Hiremagalur <abhijit@hiremaga.com>
2013-05-18 17:16:35 -0700

    Replace inline styles, flatten selectors

--- source/javascripts/all.js
+++ source/javascripts/all.js
@@ -7,5 +7,5 @@
   var today = new Date();
   var one_day_in_milliseconds = (1000 * 60 * 24 * 60);
   var days_until_conference = Math.ceil((conference_date - today) / one_day_in_milliseconds);
-  $('#days_until_conference #number').html(days_until_conference);
+  $('.countdown-number').html(days_until_conference);
 })()
