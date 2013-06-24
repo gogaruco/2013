e75b2f4b1f82592ddd659dae2b1336bb9147009e
James Hart <hjhart@gmail.com>
2013-06-23 19:35:34 -0700

    Getting around section!!

--- source/javascripts/all.js
+++ source/javascripts/all.js
@@ -1,5 +1,6 @@
 //= require jquery
 //= require foundation
+//= require google_maps
 //= require_tree .
 
 (function() {
@@ -9,3 +10,24 @@
   var days_until_conference = Math.ceil((conference_date - today) / one_day_in_milliseconds);
   $('.countdown-number').html(days_until_conference);
 })()
+
+$(document).ready(function() {
+  if($("#directions-map") !== null) {
+    var map = new google.maps.Map(document.getElementById('directions-map'), {
+        zoom: 15,
+        scrollwheel: false,
+        navigationControl: false,
+        mapTypeControl: false,
+        scaleControl: false,
+        draggable: false
+    });
+
+    var center = new google.maps.LatLng(37.76780, -122.40062);
+    var conferenceCenter = new google.maps.LatLng(37.76781, -122.39337);
+    map.setCenter(center);
+    new google.maps.Marker({
+       position: conferenceCenter,
+       map: map
+    });
+  }
+});
